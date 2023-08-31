a7a6a72499f9f6c2568f3a86af6ab20d0cce0682
pinguinsemrim <101476885+pinguinsemrim@users.noreply.github.com>
2023-08-31 12:29:42 -0300

    Add files via upload

---
+++ GameTCC/Cloneisaac/src/entities/Entity.java
@@ -0,0 +1,97 @@
+package entities;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import Game.Camera;
+import Game.World;
+import Game.Game;
+public class Entity {
+
+	protected  double x;
+	protected double y;
+	protected int width;
+	protected int height;
+	protected BufferedImage sprite;
+	public int maskx,masky,mwidth,mheight;
+	public static BufferedImage slime = Game.slime.getSprite(0,0,34,32);
+	public static BufferedImage flower = Game.flower.getSprite(0,0,40,40);
+	public static BufferedImage flower2 = Game.flower.getSprite(0,0,40*2,40);
+	public Entity (int x,int y,int width,int height,BufferedImage sprite) {
+	this.x = x ;	
+	this.y = y;
+	this.width = width;
+	this.height =height;
+	this.sprite =sprite;
+	this.maskx = 0;
+	this.masky = 0;
+	this.mwidth = width;
+	this.mheight = height;
+	}
+
+	public void setMask(int maskx,int masky,int mwidth,int mheight){
+		this.maskx = maskx;
+		this.masky = masky;
+		this.mwidth = mwidth;
+		this.mheight = mheight;
+	}
+
+	public void updateCamera() {
+		Camera.x = Camera.clamp(this.getX() - (Game.WIDTH/2),0,World.WIDTH*Game.world.TILE_SIZE - Game.WIDTH);
+		Camera.y = Camera.clamp(this.getY() - (Game.HEIGHT/2),0,World.HEIGHT*Game.world.TILE_SIZE - Game.HEIGHT);
+	}
+	public void setX(int newX) {
+		this.x=newX;
+	}
+	public void setY(int newY) {
+		this.x=newY;
+	}
+	
+
+	public int getX() {
+			return (int)this.x;
+		}
+		public int getY() {
+			return (int)this.y;
+		}
+		public int getWidth() {
+			return this.width;
+		}
+		public int getHeight() {
+			return this.height;
+		}
+		public void tick(){}
+			
+		public double calculateDistance(int x1,int y1,int x2,int y2) {
+			return Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+		}
+		
+		public static boolean isColidding(Entity e1,Entity e2){
+			Rectangle e1Mask = new Rectangle(e1.getX()+ e1.maskx,e1.getY()+e1.masky,e1.mwidth,e1.mheight);
+			Rectangle e2Mask = new Rectangle(e2.getX() + e2.maskx,e2.getY()+e2.masky,e2.mwidth,e2.mheight);
+			
+			return e1Mask.intersects(e2Mask);
+		}
+		public boolean isColiddingfora(Entity e1,int x,int y,ArrayList<Entity> e2){
+			Rectangle e1Mask = new Rectangle(x,y,e1.mwidth,e1.mheight);
+			for(int i=0;i<e2.size();i++) {
+			Rectangle e2Mask = new Rectangle(e2.get(i).getX() + e2.get(i).maskx,e2.get(i).getY()+e2.get(i).masky,e2.get(i).mwidth,e2.get(i).mheight);
+			return e1Mask.intersects(e2Mask);}
+			return false;
+			}
+		
+		public void render(Graphics2D g) {
+		g.drawImage(sprite,this.getX()-Camera.x,this.getY()-Camera.y,this.getWidth(),this.getHeight(),null);	
+		//this.render(g);
+		}
+
+		
+		
+		
+		
+		
+	}
+
+
---
+++ GameTCC/Cloneisaac/src/entities/Player.java
@@ -0,0 +1,87 @@
+package entities;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import Game.Camera;
+import Game.Game;
+import Game.World;
+
+
+public class Player extends Entity {
+		public boolean right,up,left,down;
+		public boolean aright,aup,aleft,adown;
+		public int ma=30,a=0;
+		public boolean ab=true,lr=false,colu=false,cold=false,coll=false,colr=false;
+		public double damage=1,speed=2.2;
+		public int vida =6,maxvida = 6;
+		boolean dam=false;
+		public Player(int x, int y, int width, int height,BufferedImage sprite) {
+			super(x, y, width, height, sprite);
+			
+		}
+	public void tick(){
+		if(up &&!this.isColiddingfora(this, (int)x, (int)(y-(1+speed/2)),Game.colision)) {
+			y-=speed;
+		}else if(down &&!this.isColiddingfora(this, (int)x, (int)(y+(1+speed/2)),Game.colision)) {
+			y+=speed;
+		}if(left &&!this.isColiddingfora(this, (int)(x-(1+speed/2)), (int)y,Game.colision)) {
+			x-=speed;
+		}else if(right &&!this.isColiddingfora(this, (int)(x+(1+speed/2)), (int)y,Game.colision)) {
+			x+=speed;
+		}
+		if(aup && ab) {
+			atira(3);
+			ab=false;
+		}else if(adown && ab) {
+			atira(2);
+			ab=false;
+		}else if(aleft && ab) {
+			atira(0);
+			ab=false;
+		}else if(aright && ab){
+			atira(1);
+			ab=false;
+		}
+		if(ab==false) {
+			a++;
+			if(a==ma) {
+				ab=true;
+				a=0;
+			}
+		}
+		if(vida <=0) {
+			Game.frame.dispose();
+		}
+
+    	Camera.x =Camera.clamp(this.getX()-(Game.WIDTH/2),0,World.WIDTH*Game.world.TILE_SIZE-Game.WIDTH);
+    	Camera.y =Camera.clamp(this.getY()-(Game.HEIGHT/2),0,World.HEIGHT*Game.world.TILE_SIZE-Game.HEIGHT);
+
+	}
+	public void damege(double dama) {
+		dam=true;
+		vida-=dama;
+	}
+	public void atira(int dir) {
+		Shoot s = new Shoot(this.getX()-Camera.x,this.getY()-Camera.y, 5, 5,damage);
+		if(lr) {
+		s = new Shoot(Game.player.getX()+5,Game.player.getY()+7, 5, 5,damage);
+			lr = false;
+		}else{
+			s = new Shoot(Game.player.getX()+5,Game.player.getY()+3, 5, 5,damage);
+			lr=true;
+		}
+		s.dir = dir;
+		Game.shoot.add(s);
+	}
+
+	
+	public void render(Graphics g) {
+		if(!dam) {
+			g.setColor(Color.BLUE);
+			}else {
+			g.setColor(Color.WHITE);
+			dam=false;
+			}
+	g.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
+	}}
